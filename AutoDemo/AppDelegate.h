17bb2d25d478c23b9bcf6bebe96c28d8af8d8732
余国华 <57159354@qq.com>
2019-06-21 09:56:45 +0800

    项目初始化

---
+++ AutoDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  AutoDemo
+//
+//  Created by ghyud on 2019/6/21.
+//  Copyright © 2019 ghyud. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ AutoDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  AutoDemo
+//
+//  Created by ghyud on 2019/6/21.
+//  Copyright © 2019 ghyud. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
